cf0e00871fe2c81f835daac82ea977619aa18f82
quanglongle913 <quanglongle913@gmail.com>
2023-05-22 21:25:19 +0700

    qeqeqr

--- Assets/_Game/Scripts/Player.cs
+++ Assets/_Game/Scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : Character
     [SerializeField] private FloatingJoystick floatingJoystick;
     [SerializeField] private float moveSpeed = 5.0f;
 
-    [Header ("Player Step Clinmb:")]
+    [Header("Player Step Clinmb:")]
     [SerializeField] GameObject stepRayLower;
     //[SerializeField] GameObject stepRayUpper;
     //[SerializeField] float stepHeight = 0.3f;
@@ -17,18 +17,21 @@ public class Player : Character
 
     private float horizontal;
     private float vertical;
+    private float posY;
+    public float PosY { get => posY; set => posY = value; }
     public override void Awake()
     {
         base.Awake();
+        posY = 0;
     }
     public override void OnInit()
     {
         base.OnInit();
-      
+
     }
     public void FixedUpdate()
     {
-       
+
         horizontal = floatingJoystick.Horizontal;
         vertical = floatingJoystick.Vertical;
 
@@ -42,84 +45,101 @@ public class Player : Character
             {
                 ChangeAnim("Idle");
             }
-            //Chuyển nhân vật về sát mặt đất
-            if (!CheckGrounded(0.05f))
-            {
-                //Debug.Log("Low1 ");
-                _rigidbody.transform.position = new Vector3(_rigidbody.transform.position.x, _rigidbody.transform.position.y - 0.02f, _rigidbody.transform.position.z);
-            }
+            ////Chuyển nhân vật về sát mặt đất
+            //if (!CheckGrounded(0.1f))
+            //{
+            //    //Debug.Log("Low1 ");
+            //    _rigidbody.transform.position = new Vector3(_rigidbody.transform.position.x, _rigidbody.transform.position.y - 0.045f, _rigidbody.transform.position.z);
+            //}
         }
+        else ChangeAnim("Idle");
 
-        else
-        {
-            //Debug.Log("High ");
-            _rigidbody.transform.position = new Vector3(_rigidbody.transform.position.x, _rigidbody.transform.position.y - 0.1f, _rigidbody.transform.position.z); ;
-        }
+        //else if (CheckGrounded(1.5f))
+        //{
+        //    //Debug.Log("High ");
+        //    _rigidbody.transform.position = new Vector3(_rigidbody.transform.position.x, _rigidbody.transform.position.y - 0.5f, _rigidbody.transform.position.z); ;
+        //}
 
     }
     private void Moving(float _horizontal, float _vertical)
     {
-        //StepOffset =0.3f
-        Vector3 _Direction = new Vector3(_horizontal * moveSpeed, _rigidbody.velocity.y, _vertical * moveSpeed);
-        TargetPoint = new Vector3(_rigidbody.position.x + _Direction.x, _rigidbody.position.y, _rigidbody.position.z + _Direction.z);
-        RotateTowards(this.gameObject, _Direction);
-        if (!isWall(Constant.RAYCAST_HIT_RANGE_WALL, LayerMask.GetMask(Constant.LAYER_WALL_FLOOR)) && !isWall(Constant.RAYCAST_HIT_RANGE_WALL,LayerMask.GetMask(Constant.LAYER_WALL_STAIR_BRICK)))
+        if (CheckGrounded(0.4f))
         {
-            float stepOffet=0.4f;
-            float _hitRange = 0.2f;
-            //Đi lên cầu thang 
-            RaycastHit hit;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hit, _hitRange, LayerMask.GetMask(Constant.LAYER_GROUND)))
+            //StepOffset =0.3f
+            Vector3 _Direction = new Vector3(_horizontal * moveSpeed, _rigidbody.velocity.y, _vertical * moveSpeed);
+            TargetPoint = new Vector3(_rigidbody.position.x + _Direction.x, _rigidbody.position.y + posY, _rigidbody.position.z + _Direction.z);
+            RotateTowards(this.gameObject, _Direction);
+            if (!isWall(Constant.RAYCAST_HIT_RANGE_WALL, LayerMask.GetMask(Constant.LAYER_WALL_FLOOR)))
             {
-                //Debug.Log("Did Hit");
-                TargetPoint = hit.collider.transform.position;
-                TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
-                moveTarget(TargetPoint);
+                float stepOffet = 0.4f;
+                float _hitRange = 0.1f;
+                //Đi lên cầu thang 
+                //RaycastHit hit;
+                //if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hit, _hitRange, LayerMask.GetMask(Constant.LAYER_STAIR_BRICK)))
+                //{
+                //    //Debug.Log("Did Hit");
+                //    TargetPoint = hit.collider.transform.position;
+                //    TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
+                //    moveTarget(TargetPoint);
 
-            }
-            RaycastHit hitLower45;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, _hitRange, LayerMask.GetMask(Constant.LAYER_GROUND)))
-            {
-                //Debug.Log("Did hitLower45");
+                //}
+                //RaycastHit hitLower45;
+                //if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, _hitRange, LayerMask.GetMask(Constant.LAYER_STAIR_BRICK)))
+                //{
+                //    //Debug.Log("Did hitLower45");
 
-                TargetPoint = hitLower45.collider.transform.position;
-                TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
-                moveTarget(TargetPoint);
-            }
+                //    TargetPoint = hitLower45.collider.transform.position;
+                //    TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
+                //    moveTarget(TargetPoint);
+                //}
 
-            RaycastHit hitLowerMinus45;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, _hitRange, LayerMask.GetMask(Constant.LAYER_GROUND)))
-            {
-               // Debug.Log("Did hitLowerMinus45");
-                TargetPoint = hitLowerMinus45.collider.transform.position;
-                TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
-                moveTarget(TargetPoint);
-            }
-           
-            else
-            {
+                //RaycastHit hitLowerMinus45;
+                //if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, _hitRange, LayerMask.GetMask(Constant.LAYER_STAIR_BRICK)))
+                //{
+                //    // Debug.Log("Did hitLowerMinus45");
+                //    TargetPoint = hitLowerMinus45.collider.transform.position;
+                //    TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
+                //    moveTarget(TargetPoint);
+                //}
+
+                //else
+                //{
                 // di chuyển bình thường
-                _rigidbody.transform.position = Vector3.MoveTowards(_rigidbody.transform.position, TargetPoint, moveSpeed * Time.fixedDeltaTime);
+                _rigidbody.transform.position = new Vector3(TargetPoint.x, TargetPoint.y, TargetPoint.z);
+                //_rigidbody.transform.position = Vector3.MoveTowards(_rigidbody.transform.position, TargetPoint, moveSpeed * Time.fixedDeltaTime);
+                //}
+                ChangeAnim("Run");
             }
-            ChangeAnim("Run");
         }
+        else ChangeAnim("Idle");
     }
-    private void moveTarget(Vector3 _TargetPoint)
+    public void moveTarget(float posY)
     {
-        _rigidbody.transform.position = new Vector3(_rigidbody.transform.position.x, TargetPoint.y, _rigidbody.transform.position.z);
-        _rigidbody.transform.position = Vector3.MoveTowards(_rigidbody.transform.position, _TargetPoint, moveSpeed * Time.fixedDeltaTime);
+        PosY = posY;
+        //_rigidbody.transform.position = new Vector3(_rigidbody.transform.position.x, _TargetPoint.y, _rigidbody.transform.position.z);
+        //_rigidbody.transform.position = Vector3.MoveTowards(_rigidbody.transform.position, _TargetPoint, moveSpeed * Time.fixedDeltaTime);
+        //EDIT
+        //Debug.Log("_TargetPoint.y:= " + _TargetPoint.y);
+        //_rigidbody.transform.position = new Vector3(TargetPoint.x, TargetPoint.y, TargetPoint.z);
     }
-    private bool isWall(float _hitRange,LayerMask _layerMask)
+    private bool isWall(float _hitRange, LayerMask _layerMask)
     {
         RaycastHit hit;
         bool isWall = false;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, _hitRange, _layerMask))
         {
             isWall = true;
+            //if (_layerMask == LayerMask.GetMask(Constant.LAYER_WALL_STAIR_BRICK))
+            //{
+            //    if (BrickCount > 0)
+            //    {
+            //        hit.collider.gameObject.SetActive(false);
+            //    }
+            //}
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
             //Debug.Log("Did Hit");
         }
-        else 
+        else
         {
             isWall = false;
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
@@ -131,6 +151,8 @@ public class Player : Character
     {
         Debug.DrawLine(transform.position, transform.position + Vector3.down * _rangeCast, Color.red);
         RaycastHit hit;
-        return Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, _rangeCast, groundLayer);
+        //return Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, _rangeCast, groundLayer);
+        //EDit...
+        return true;
     }
 }
--- Assets/_Game/Scripts/StairBrick.cs
+++ Assets/_Game/Scripts/StairBrick.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class StairBrick : MonoBehaviour
 {
+    float stepOffet = 0.5f;
     [SerializeField] private GameObject wall;
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<Character>())
+        if (other.gameObject.GetComponent<Player>())
         {
             Character _character = other.gameObject.GetComponent<Character>();
-
-            if (_character.BrickCount > 0 && wall.gameObject.activeSelf)
+            Brick BrickObject = this.gameObject.GetComponent<Brick>();
+            //UNDONE
+            if (_character.BrickCount > 0 && _character.ColorType != BrickObject.ColorType)
             {
-                wall.gameObject.SetActive(false);
+                if (_character.ColorType != BrickObject.ColorType)
+                {
+                    _character.RemoveBrick();
+                    BrickObject.ChangeColor(_character.ColorType);
+                    MeshRenderer mesh = this.gameObject.GetComponent<MeshRenderer>();
+                    mesh.enabled = true;
+                }
+                Vector3 TargetPoint = new Vector3(this.transform.position.x, this.transform.position.y + stepOffet, this.transform.position.z-0.1f);
+                Player _player = other.gameObject.GetComponent<Player>();
+                if (_player != null)
+                {
+                    _player.moveTarget(TargetPoint.y);
+                }
+            }
+            else if (_character.ColorType == BrickObject.ColorType)
+            {
+                Vector3 TargetPoint = new Vector3(this.transform.position.x, this.transform.position.y + stepOffet, this.transform.position.z - 0.1f);
+                Player _player = other.gameObject.GetComponent<Player>();
+                if (_player != null)
+                {
+                    _player.moveTarget(TargetPoint.y);
+                }
+            }
+        }
+        if (other.gameObject.GetComponent<BotAI>())
+        {
+            Character _character = other.gameObject.GetComponent<Character>();
+            Brick BrickObject = this.gameObject.GetComponent<Brick>();
+            //UNDONE
+            if (_character.BrickCount > 0 && _character.ColorType != BrickObject.ColorType)
+            {
+                //wall.gameObject.SetActive(false);
                 _character.RemoveBrick();
-                this.gameObject.GetComponent<Brick>().ChangeColor(_character.ColorType);
+                BrickObject.ChangeColor(_character.ColorType);
                 MeshRenderer mesh = this.gameObject.GetComponent<MeshRenderer>();
                 mesh.enabled = true;
 
