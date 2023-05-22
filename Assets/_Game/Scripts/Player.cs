1926b89297f27bda54624661894c545e1c328199
quanglongle913 <quanglongle913@gmail.com>
2023-05-22 15:55:58 +0700

    -UNDONE BotAI

--- Assets/_Game/Scripts/Player.cs
+++ Assets/_Game/Scripts/Player.cs
@@ -104,53 +104,6 @@ public class Player : Character
             ChangeAnim("Run");
         }
     }
-    private void Moving_rb(float _horizontal, float _vertical)
-    {
-        //StepOffset =0.3f
-        Vector3 _Direction = new Vector3(_horizontal * moveSpeed, _rigidbody.velocity.y, _vertical * moveSpeed);
-        TargetPoint = new Vector3(_rigidbody.position.x + _Direction.x, _rigidbody.position.y, _rigidbody.position.z + _Direction.z);
-        RotateTowards(this.gameObject, _Direction);
-        if (!isWall(Constant.RAYCAST_HIT_RANGE_WALL, LayerMask.GetMask(Constant.LAYER_WALL_FLOOR)) && !isWall(Constant.RAYCAST_HIT_RANGE_WALL, LayerMask.GetMask(Constant.LAYER_WALL_STAIR_BRICK)))
-        {
-            float stepOffet = 0.4f;
-            float _hitRange = 0.2f;
-            //Đi lên cầu thang 
-            RaycastHit hit;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hit, _hitRange, LayerMask.GetMask(Constant.LAYER_GROUND)))
-            {
-                //Debug.Log("Did Hit");
-                TargetPoint = hit.collider.transform.position;
-                TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
-                moveTarget(TargetPoint);
-
-            }
-            RaycastHit hitLower45;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, _hitRange, LayerMask.GetMask(Constant.LAYER_GROUND)))
-            {
-                //Debug.Log("Did hitLower45");
-
-                TargetPoint = hitLower45.collider.transform.position;
-                TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
-                moveTarget(TargetPoint);
-            }
-
-            RaycastHit hitLowerMinus45;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, _hitRange, LayerMask.GetMask(Constant.LAYER_GROUND)))
-            {
-                // Debug.Log("Did hitLowerMinus45");
-                TargetPoint = hitLowerMinus45.collider.transform.position;
-                TargetPoint = new Vector3(TargetPoint.x, TargetPoint.y + stepOffet, TargetPoint.z);
-                moveTarget(TargetPoint);
-            }
-
-            else
-            {
-                // di chuyển bình thường
-                _rigidbody.transform.position = Vector3.MoveTowards(_rigidbody.transform.position, TargetPoint, moveSpeed * Time.fixedDeltaTime);
-            }
-            ChangeAnim("Run");
-        }
-    }
     private void moveTarget(Vector3 _TargetPoint)
     {
         _rigidbody.transform.position = new Vector3(_rigidbody.transform.position.x, TargetPoint.y, _rigidbody.transform.position.z);
